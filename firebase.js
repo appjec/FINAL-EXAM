b3cef9dc42e4419b3d7a1490bf433f52063a302f
myca05 <carrillojecel@gmail.com>
2025-01-10 11:02:51 +0800

    homepage

--- firebase.js
+++ firebase.js
@@ -1,17 +1,12 @@
-   // Initialize Firebase
-   const app = initializeApp(firebaseConfig);
-   const database = getDatabase(app);
+const userList = document.getElementById('userList');
+const usersRef = ref(database, 'users');
 
-   // Display registered users
-   const userList = document.getElementById('userList');
-   const usersRef = ref(database, 'users');
-   
-   onValue(usersRef, (snapshot) => {
-     userList.innerHTML = ''; // Clear the list before adding new data
-     snapshot.forEach((childSnapshot) => {
-       const user = childSnapshot.val();
-       const listItem = document.createElement('li');
-       listItem.textContent = `Name: ${user.username}, Email: ${user.email}`;
-       userList.appendChild(listItem);
-     });
-   });
+onValue(usersRef, (snapshot) => {
+  userList.innerHTML = ''; // Clear the list before adding new data
+  snapshot.forEach((childSnapshot) => {
+    const user = childSnapshot.val();
+    const listItem = document.createElement('li');
+    listItem.textContent = `Name: ${user.username}, Email: ${user.email}`;
+    userList.appendChild(listItem);
+  });
+});
